59718ad54ad2f4e3017bc057d656e2b397282a47
Pilar Figueroa <pilar.fc@outlook.com>
2017-03-28 19:36:10 -0600

    Trate de hacer algo con la pelea pero no pude

--- pokedex.js
+++ pokedex.js
@@ -6,12 +6,13 @@ function Pokemon(nombre,color,puntosAtaque){
   this.puntosAtaque = puntosAtaque;
 
    /* this.saludar = function(){
-    console.log("Hola, me llamo " + this.nombre) */
-  };
+    console.log("Hola, me llamo " + this.nombre)
+  }; */
 
   this.pelear = function(pokemonObjeto){
     pokemonObjeto.vida = pokemonObjeto.vida - this.puntosAtaque
   }
+}
 
 /*  this.beber = function(tipoAlcohol){
     if(tipoAlcohol == "chelas"){
@@ -56,6 +57,23 @@ function crearPokemon () {
     lista.appendChild(pokemonesCreados);
     var listaPokemons = document.getElementById("pokemones2").appendChild(lista);
  }
+
+
+   /*function ataque1 () {
+     if (vidaPokemon1 > 0 ) {
+       vidaPokemon1 = vidaPokemon1 - ataque;
+     } else {
+
+     } alert ("Pokemon murió")
+   }
+
+   function ataque2 () {
+     if (vidaPokemon1 > 0 ) {
+       vidaPokemon1 = vidaPokemon1 - ataque;
+     } else {
+
+     } alert ("Pokemon murió")
+   } */
 
    function borrar () {
       var stringVacio = "";
